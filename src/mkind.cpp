8dcf00cc44f54106189a654d6113e4632f9a4062
tomohxx <43703429+tomohxx@users.noreply.github.com>
2026-04-11 20:52:54 +0900

    refactor: use cbegin/cend in std::for_each for const correctness

--- src/mkind.cpp
+++ src/mkind.cpp
@@ -137,12 +137,12 @@ int main()
 
     std::vector<std::array<uint32_t, 10>> dists(hands.size(), std::array<uint32_t, 10>{});
 
-    std::for_each(POLICY, hands.begin(), hands.end(),
+    std::for_each(POLICY, hands.cbegin(), hands.cend(),
                   [&deltas, &dists](const auto& hand_hash) {
                     dp<9>(hand_hash.first, deltas, dists[hand_hash.second]);
                   });
 
-    std::for_each(dists.begin(), dists.end(),
+    std::for_each(dists.cbegin(), dists.cend(),
                   [&fout](const auto& dist) {
                     fout.write(reinterpret_cast<const char*>(dist.data()), dist.size() * sizeof(uint32_t));
                   });
@@ -171,12 +171,12 @@ int main()
 
     std::vector<std::array<uint32_t, 10>> dists(hands.size(), std::array<uint32_t, 10>{});
 
-    std::for_each(POLICY, hands.begin(), hands.end(),
+    std::for_each(POLICY, hands.cbegin(), hands.cend(),
                   [&deltas, &dists](const auto& hand_hash) {
                     dp<7>(hand_hash.first, deltas, dists[hand_hash.second]);
                   });
 
-    std::for_each(dists.begin(), dists.end(),
+    std::for_each(dists.cbegin(), dists.cend(),
                   [&fout](const auto& dist) {
                     fout.write(reinterpret_cast<const char*>(dist.data()), dist.size() * sizeof(uint32_t));
                   });
